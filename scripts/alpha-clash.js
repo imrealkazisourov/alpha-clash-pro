9be4fbdf9b77196f201953fcff1323f1cd0b965f
Kazi Sourov <iamkazisourov@gmail.com>
2024-06-20 06:55:48 +0600

    added javascript functions to show and hide the screens

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,9 +1,4 @@
 function playNow() {
-    //hide home section after clicking 
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden');
-
-    //show playground section
-    const playGroundSection = document.getElementById('play-ground');
-    playGroundSection.classList.remove('hidden');
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
