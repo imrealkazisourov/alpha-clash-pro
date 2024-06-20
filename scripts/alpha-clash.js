b1a9e490f034e87dbb2e06d3fa72c8cefe1ff47e
Kazi Sourov <iamkazisourov@gmail.com>
2024-06-21 05:18:32 +0600

    added score functions

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,42 @@
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log(playerPressed);
+    
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(currentAlphabet, expectedAlphabet);
+
+    //check matched or not 
+    if (playerPressed === expectedAlphabet) {
+        //update score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        // new score
+        const newScore = currentScore + 1;
+
+        currentScoreElement.innerText = newScore;
+        
+
+        // start a new round
+        removeKeyHighlightColor(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const updateLife = currentLife - 1;
+
+        currentLifeElement.innerText = updateLife;
+    }
+}
+
+// capture keyboard press
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
 // Game Continue Function 
 function continueGame() {
     //generating random alphabet is first step 
