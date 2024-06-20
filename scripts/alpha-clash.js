a258204694a9bcd443dd0c6c4fce27ecea8748f2
Kazi Sourov <iamkazisourov@gmail.com>
2024-06-20 07:28:18 +0600

    added functions to generate random alphabet and show the alphabet to the screen

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,20 @@
+// Game Continue Function 
+function continueGame() {
+    //generating random alphabet is first step 
+    const alphabet = getRandAlphabet();
+    console.log(alphabet);
+
+    //set randomly generated alphabet to the screen (show it)
+    
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    
+}
+
+// home screen to game screen function 
 function playNow() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,8 +1,27 @@
+// hiding screen function 
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
+// showing the element by id
 function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+
+// random alphabet generating function 
+function getRandAlphabet(){
+    //creating alphabet array
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    // get a random index between 0-25 because english alphabets are 26 and index starts from 0
+
+    const randomNumber = Math.random()*25;
+    const randomIndex = Math.round(randomNumber);
+    const alphabet = alphabets[randomIndex];
+    // console.log(randomIndex, alphabet);
+    return alphabet;
+    
 }
